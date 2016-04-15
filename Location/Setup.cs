d66dc208e94b2dbcef26e35ce31bd8598e73d39a
Hernan Zaldivar <hernan.zaldivar@neoris.cxnetworks.net>
2016-04-15 17:01:45 -0300

    Mvx.LazyConstructAndRegisterSingleton<IConfiguration, Configuration>(); Mvx.RegisterType<ILocationManager, LocationManager>();

--- Location/Setup.cs
+++ Location/Setup.cs
@@ -1,7 +1,10 @@
-﻿using Location.Core;
+﻿using System;
+using System.IO;
+using Location.Core;
 using MvvmCross.Core.ViewModels;
 using MvvmCross.iOS.Platform;
 using MvvmCross.iOS.Views.Presenters;
+using MvvmCross.Platform;
 
 namespace Location
 {
@@ -14,7 +17,29 @@ namespace Location
 
         protected override IMvxApplication CreateApp()
         {
+            InitializeContainers();
+
+            SetupConfiguration();
+
             return new App();
+        }
+
+        private void SetupConfiguration()
+        {
+            var configuration = Mvx.Resolve<IConfiguration>();
+
+            // we need to put in /Library/ on iOS5.1 to meet Apple's iCloud terms
+            // (they don't want non-user-generated data in Documents)
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
+            configuration.DbPath = Path.Combine(documentsPath, "../Library/");
+
+            configuration.SQLitePlatform = new SQLite.Net.Platform.XamarinIOS.SQLitePlatformIOS();
+        }
+
+        private void InitializeContainers()
+        {
+            Mvx.LazyConstructAndRegisterSingleton<IConfiguration, Configuration>();
+            Mvx.RegisterType<ILocationManager, LocationManager>();
         }
     }
 }
