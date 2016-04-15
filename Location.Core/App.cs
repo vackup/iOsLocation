28e4b059d41d39255ffc8d8336a7e4f965ff1ba1
Hernan Zaldivar <hernan.zaldivar@neoris.cxnetworks.net>
2016-04-15 18:08:40 -0300

    - Init db

--- Location.Core/App.cs
+++ Location.Core/App.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using Location.Core.ViewModels;
 using Location.Dao;
 using Location.Models;
@@ -14,10 +15,24 @@ namespace Location.Core
     {
         public App()
         {
+        }
+
+        public override async void Initialize()
+        {
             Mvx.LazyConstructAndRegisterSingleton<IRepository<DeviceLocation>, Repository<DeviceLocation>>();
             Mvx.RegisterType<IDatabase, Database>();
 
             Mvx.RegisterSingleton<IMvxAppStart>(new MvxAppStart<LocationViewModel>());
+
+            await SetupDataBase();
+        }
+
+        private async Task SetupDataBase()
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            var context = Mvx.Resolve<IDatabase>();
+            await context.InitializeSQLiteAsync(cancellationTokenSource.Token);
         }
     }
 }
